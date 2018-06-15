aca9b367dcdf92d0ca173d8101bcf431466d5666
TechnoVisual <mark@technovisual.co.uk>
2018-06-15 20:46:01 +0100

    Updated formatting
    Updated Formatting

--- race1.py
+++ race1.py
@@ -1,41 +1,26 @@
-# PyGame Zero Racing Game
 from random import randint
-
-# First set the width and height of the window
-WIDTH = 700
-HEIGHT = 600
-
-# Load in the car sprite image as an Actor object
-car = Actor("racecar")
-car.pos = 250, 500 # Set the car screen position
-
-# Some variables to control the track
+WIDTH = 700 # Width of window
+HEIGHT = 800 # Height of window
+car = Actor("racecar") # Load in the car Actor image
+car.pos = 250, 700 # Set the car screen position
 SPEED = 4
 trackCount = 0
 trackPosition = 250
 trackWidth = 120
 trackDirection = False
-# The following variables set up the track sprites
-trackLeft = []
-trackRight = [] 
-# Variable to track the status of the game
+trackLeft = []  # list of track barriers left
+trackRight = []  # list of track barriers right
 gameStatus = 0
 
-# Pygame Zero draw function 
-def draw():
+def draw(): # Pygame Zero draw function
     global gameStatus
     screen.fill((128, 128, 128))
     if gameStatus == 0:
         car.draw()
         b = 0
         while b < len(trackLeft):
-            if car.colliderect(trackLeft[b]) or car.colliderect(trackRight[b]):
-                # Red flag time
-                gameStatus = 1
             trackLeft[b].draw()
-            trackLeft[b].y += SPEED
             trackRight[b].draw()
-            trackRight[b].y += SPEED
             b += 1
     if gameStatus == 1:
         # Red Flag
@@ -44,47 +29,38 @@ def draw():
         # Chequered Flag
         screen.blit('cflag', (318, 268))
 
-# Pygame Zero update function
-def update():
+def update(): # Pygame Zero update function
     global gameStatus , trackCount
     if gameStatus == 0:
-        if keyboard.left:
-            car.x -= 2
-        elif keyboard.right:
-            car.x += 2
+        if keyboard.left: car.x -= 2
+        if keyboard.right: car.x += 2
         updateTrack()
-    if trackCount > 200:
-        # Chequered flag time
-        gameStatus = 2
+    if trackCount > 200: gameStatus = 2 # Chequered flag state
 
-# Our game functions
-
-# Function to make a new section of track
-def makeTrack():
+def makeTrack(): # Function to make a new section of track
     global trackCount, trackLeft, trackRight, trackPosition, trackWidth
     trackLeft.append(Actor("barrier", pos = (trackPosition-trackWidth,0)))
     trackRight.append(Actor("barrier", pos = (trackPosition+trackWidth,0)))
-    trackCount += 1
-    
-# Function to update where the track blocks appear
-def updateTrack():
-    global trackCount, trackPosition, trackDirection, trackWidth 
+    trackCount += 1   
+
+def updateTrack(): # Function to update where the track blocks appear
+    global trackCount, trackPosition, trackDirection, trackWidth, gameStatus
+    b = 0
+    while b < len(trackLeft):
+        if car.colliderect(trackLeft[b]) or car.colliderect(trackRight[b]):
+            gameStatus = 1  # Red flag state
+        trackLeft[b].y += SPEED
+        trackRight[b].y += SPEED
+        b += 1 
     if trackLeft[len(trackLeft)-1].y > 32:
-        if trackDirection == False:
-            trackPosition += 16
-        if trackDirection == True:
-            trackPosition -= 16
-            
-        if randint(0, 4) == 1:
-            trackDirection = not trackDirection
-        if trackPosition > 700-trackWidth:
-            trackDirection = True
-        if trackPosition < trackWidth:
-            trackDirection = False
+        if trackDirection == False: trackPosition += 16
+        if trackDirection == True: trackPosition -= 16
+        if randint(0, 4) == 1: trackDirection = not trackDirection
+        if trackPosition > 700-trackWidth: trackDirection = True
+        if trackPosition < trackWidth: trackDirection = False
         makeTrack()
                             
 # End of functions
-                
 makeTrack() # Make first block of track
 
 	
